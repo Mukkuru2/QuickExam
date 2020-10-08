f7e0a8bc779f27a21474870adf65c925b80e44dd
Dorus van weelden <dorusvanweelden@gmail.com>
2020-10-08 19:09:19 +0200

    added edit to database

--- controllers/landing.js
+++ controllers/landing.js
@@ -1,10 +1,10 @@
 const models = require("../models");
 
 exports.get_landing = function (req, res, next) {
-    res.render('landing', { title: 'Express' });
+    res.render('landing', { title: 'Email test!' });
 }
 
-exports.submit_adress = function (req, res, next) {
+exports.submit_email = function (req, res, next) {
 
     return models.TestTable.create({
         email: req.body.email_submitted
@@ -15,7 +15,7 @@ exports.submit_adress = function (req, res, next) {
 
 exports.show_emails = function (req, res, next) {
     return models.TestTable.findAll().then(emails => {
-        res.render('landing', { title: 'Express', emails: emails });
+        res.render('landing', { title: 'Email test!', emails: emails });
     })
 }
 
@@ -25,6 +25,29 @@ exports.show_email = function (req, res, next) {
             id: req.params.email_id
         }
     }).then(email => {
-        res.render('email', { email : email });
+        res.render('email', { email: email });
+    })
+}
+
+exports.show_edit_email = function (req, res, next) {
+    return models.TestTable.findOne({
+        where: {
+            id: req.params.email_id
+        }
+    }).then(email => {
+        res.render('email/edit_email', { email: email });
+    })
+}
+
+exports.edit_email = function (req, res, next) {
+    console.log(req.body.email, req.params.email_id);
+    return models.TestTable.update({
+        email: req.body.email_submitted
+    }, {
+        where: {
+            id: req.params.email_id
+        }
+    }).then(result => {
+        res.redirect('/email/' + req.params.email_id);
     })
 }
--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,19 @@ let landing = require('../controllers/landing.js')
 
 /* GET home page. */
 router.get('/', landing.get_landing);
-router.post('/', landing.submit_adress);
+
+/* Submit an email */
+router.post('/', landing.submit_email);
+
+/* GET all emails */
 router.get('/emails', landing.show_emails)
+
+/* GET individual email */
 router.get('/email/:email_id', landing.show_email)
+
+/* GET edit and post an email */
+router.get('/email/:email_id/edit', landing.show_edit_email)
+router.post('/email/:email_id/edit', landing.edit_email)
+
 
 module.exports = router;
