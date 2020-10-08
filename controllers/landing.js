c26afde1681935fee5879f53a7e14dd93742514b
Dorus van weelden <dorusvanweelden@gmail.com>
2020-10-08 18:13:32 +0200

    can now read database data

--- controllers/landing.js
+++ controllers/landing.js
@@ -1,6 +1,6 @@
 const models = require("../models");
 
-exports.get_landing = function(req, res, next) {
+exports.get_landing = function (req, res, next) {
     res.render('landing', { title: 'Express' });
 }
 
@@ -9,6 +9,22 @@ exports.submit_adress = function (req, res, next) {
     return models.TestTable.create({
         email: req.body.email_submitted
     }).then(email => {
-        res.redirect('/');
+        res.redirect('/emails');
+    })
+}
+
+exports.show_emails = function (req, res, next) {
+    return models.TestTable.findAll().then(emails => {
+        res.render('landing', { title: 'Express', emails: emails });
+    })
+}
+
+exports.show_email = function (req, res, next) {
+    return models.TestTable.findOne({
+        where: {
+            id: req.params.email_id
+        }
+    }).then(email => {
+        res.render('email', { email : email });
     })
 }
--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,7 @@ let landing = require('../controllers/landing.js')
 /* GET home page. */
 router.get('/', landing.get_landing);
 router.post('/', landing.submit_adress);
+router.get('/emails', landing.show_emails)
+router.get('/email/:email_id', landing.show_email)
 
 module.exports = router;
